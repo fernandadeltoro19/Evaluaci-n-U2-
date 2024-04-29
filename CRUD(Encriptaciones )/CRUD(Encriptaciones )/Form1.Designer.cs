2a5c7d09ee77e3d840ef10628d3da7e13e3246d9
Del Toro Lira Fernanda <i20050585@monclova.tecnm.mx>
2024-04-28 22:32:04 -0600

    Initial Commit

---
+++ CRUD(Encriptaciones )/CRUD(Encriptaciones )/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace CRUD_Encriptaciones__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nombre = new System.Windows.Forms.Label();
+            this.apellido = new System.Windows.Forms.Label();
+            this.Correo = new System.Windows.Forms.Label();
+            this.contrasena = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nombre
+            // 
+            this.nombre.AutoSize = true;
+            this.nombre.Location = new System.Drawing.Point(25, 79);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(44, 13);
+            this.nombre.TabIndex = 0;
+            this.nombre.Text = "Nombre";
+            // 
+            // apellido
+            // 
+            this.apellido.AutoSize = true;
+            this.apellido.Location = new System.Drawing.Point(25, 133);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(44, 13);
+            this.apellido.TabIndex = 1;
+            this.apellido.Text = "Apellido";
+            // 
+            // Correo
+            // 
+            this.Correo.AutoSize = true;
+            this.Correo.Location = new System.Drawing.Point(25, 182);
+            this.Correo.Name = "Correo";
+            this.Correo.Size = new System.Drawing.Size(38, 13);
+            this.Correo.TabIndex = 2;
+            this.Correo.Text = "Correo";
+            // 
+            // contrasena
+            // 
+            this.contrasena.AutoSize = true;
+            this.contrasena.Location = new System.Drawing.Point(25, 233);
+            this.contrasena.Name = "contrasena";
+            this.contrasena.Size = new System.Drawing.Size(61, 13);
+            this.contrasena.TabIndex = 3;
+            this.contrasena.Text = "Contrasena";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(111, 79);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(111, 126);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(111, 179);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(100, 20);
+            this.txtCorreo.TabIndex = 6;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(111, 230);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(100, 20);
+            this.txtContrasena.TabIndex = 7;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(136, 325);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 8;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(342, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(697, 250);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(239, 131);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MD5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(239, 169);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "SHA1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(239, 210);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 17);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "SHA2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1241, 578);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.contrasena);
+            this.Controls.Add(this.Correo);
+            this.Controls.Add(this.apellido);
+            this.Controls.Add(this.nombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nombre;
+        private System.Windows.Forms.Label apellido;
+        private System.Windows.Forms.Label Correo;
+        private System.Windows.Forms.Label contrasena;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ CRUD(Encriptaciones )/CRUD(Encriptaciones )/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CRUD_Encriptaciones__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CargarDatos();
+        }
+
+        private MySqlConnection conexion;
+        string cadenaConexion = "Server=localhost;Database=prueba;Uid=root;Pwd=;";
+
+        private void CargarDatos()
+        {
+            conexion = new MySqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                string consulta = "SELECT * FROM prueba2"; // Cambia prueba2 por el nombre de tu tabla
+                MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(comando);
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                dataGridView1.DataSource = tabla; // Enlazamos el DataGridView con la tabla
+
+                // Opcional: ajustar las columnas automáticamente para que se vean todos los datos
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string correo = txtCorreo.Text;
+            string contrasena = txtContrasena.Text;
+
+            // Validar que los campos no estén vacíos
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido) ||
+                string.IsNullOrEmpty(correo) || string.IsNullOrEmpty(contrasena))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+                return;
+            }
+
+            // Determinar qué tipo de encriptación se utilizará
+            string tipoEncriptacion = "";
+            if (radioButton1.Checked)
+            {
+                tipoEncriptacion = "MD5";
+            }
+            else if (radioButton2.Checked)
+            {
+                tipoEncriptacion = "SHA1";
+            }
+            else if (radioButton3.Checked)
+            {
+                tipoEncriptacion = "SHA256";
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un tipo de encriptación.");
+                return;
+            }
+
+
+
+            // Aplicar la encriptación al campo de contraseña
+            string contrasenaEncriptada = EncriptarContrasena(contrasena, tipoEncriptacion);
+
+            // Crear la conexión y realizar la inserción
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string consulta = "INSERT INTO prueba2 (nombre, apellido, correo, contrasena) " +
+                        "VALUES (@nombre, @apellido, @correo, @contrasena)";
+                    MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                    comando.Parameters.AddWithValue("@nombre", nombre);
+                    comando.Parameters.AddWithValue("@apellido", apellido);
+                    comando.Parameters.AddWithValue("@correo", correo);
+                    comando.Parameters.AddWithValue("@contrasena", contrasenaEncriptada);
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("Registro insertado correctamente.");
+                        CargarDatos();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al insertar el registro.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al insertar el registro: " + ex.Message);
+                }
+            }
+        }
+
+        private string EncriptarContrasena(string contrasena, string tipoEncriptacion)
+        {
+            switch (tipoEncriptacion)
+            {
+                case "MD5":
+                    return EncriptarMD5(contrasena);
+                case "SHA1":
+                    return EncriptarSHA1(contrasena);
+                case "SHA256":
+                    return EncriptarSHA256(contrasena);
+                default:
+                    throw new ArgumentException("Tipo de encriptación no válido.");
+            }
+        }
+
+        private string EncriptarMD5(string texto)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(texto));
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sb.Append(data[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+        private string EncriptarSHA1(string texto)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] data = sha1.ComputeHash(Encoding.UTF8.GetBytes(texto));
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sb.Append(data[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        private string EncriptarSHA256(string texto)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] data = sha256.ComputeHash(Encoding.UTF8.GetBytes(texto));
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sb.Append(data[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
